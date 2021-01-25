0d47738fd24a71a8b681a606dc3b053e9147232d
francescoganci <76791249+francescoganci@users.noreply.github.com>
2021-01-25 19:51:40 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
